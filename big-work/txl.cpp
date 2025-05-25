a1c173d8fa6f2371f4154c14f59807ba14c9c33e
M1sty <1805720779@qq.com>
2025-05-25 11:10:32 +0800

    add 1-4

--- big-work/txl.cpp
+++ big-work/txl.cpp
@@ -1,36 +1,138 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef struct Friend
+{
+    string name;
+    char id;
+}txr;
 int choice;
 char c;
+vector<txr> txl;
+
 void menu()
 {
-    cout << "1. Enter a new record" << endl;
-    cout << "2. Delete a record" << endl;
-    cout << "3. List all records" << endl;
-    cout << "4. Search for a record" << endl;
-    cout << "5. Save records to file" << endl;
-    cout << "6. Load records from file" << endl;
-    cout << "0. Exit" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << "          1. Enter a new record          " << endl;
+    cout << "          2. Delete a record             " << endl;
+    cout << "          3. List all records            " << endl;
+    cout << "          4. Search for a record         " << endl;
+    cout << "          5. Save records to file        " << endl;
+    cout << "          6. Load records from file      " << endl;
+    cout << "          0. Exit                        " << endl;
+    cout << "-----------------------------------------" << endl;
+    return ;
 }
 void enter()
 {
-
+    if(txl.size() == 0)
+    {
+        cout << "没有数据" << endl;
+    }
+    while(true)
+  {   cout << "按Y输入,按N退出" << endl;
+    cin >> c;
+    if(c == 'Y' || c == 'y')
+    {
+        txr t;
+        cout << "请输入姓名" << endl;
+        cin >> t.name;
+        cout << "请输入ID" << endl;
+        cin >> t.id;
+        txl.push_back(t);
+        cout << "输入成功" << endl;
+    }
+    else if(c == 'N' || c == 'n')
+    {
+        cout << "退出成功" << endl;
+        break;
+    }
+    else
+    {
+        cout << "输入错误" << endl;
+    }}
+    return;
 }
 void del()
 {
-
-}
+  while(true) {
+      cout << "输入要删除的联系人的姓名" << endl;
+        string name;
+        cin >> name;
+      cout << "按Enter键查询" << endl;
+        if(cin.get() == '\n')
+       {cin.ignore();
+        getline(cin, name);
+        auto p = find_if(txl.begin(), txl.end(), [&](const txr& t) { return t.name == name; });
+        if (p != txl.end())
+        {
+            cout << "按Enter键删除" << endl;
+            if(cin.get() == '\n')
+            {
+                cin.ignore();
+                 txl.erase(p);
+                cout << "删除成功" << endl;
+            }
+            else
+            {
+                cout << "输入错误" << endl;
+            }
+           
+        }
+        else
+        {
+            cout << "没有找到该联系人" << endl;
+        }}
+        cout << "按Y继续,按N退出" << endl;
+        cin >> c;
+        if(c == 'N' || c == 'n')
+        {
+            cout << "退出成功" << endl;
+            break;
+        }
+        else if(c == 'Y' || c == 'y')
+        {
+            continue;
+        }
+        else
+        {
+            cout << "输入错误" << endl;
+        }
+        return;
+}}
 void listrecord()
 {
-
+    if (txl.empty()) {
+        cout << "没有联系人记录" << endl;
+        return;
+    }
+    cout << "联系人列表：" << endl;
+    for (const auto& t : txl) {
+        cout << "姓名: " << t.name << ", ID: " << t.id << endl;
+    }
+    return;
 }
 void search()
 {
-
+    string name;
+     cout << "输入要查询的联系人的姓名" << endl;
+     cout << "按Enter键查询" << endl;
+        if(cin.get() == '\n')
+       {cin.ignore();
+        cin >> name;
+        auto p = find_if(txl.begin(), txl.end(), [&](const txr& t) { return t.name == name; });
+        if (p != txl.end())
+        {
+            cout << "姓名: " << p->name << ", ID: " << p->id << endl;
+        }
+        else
+        {
+            cout << "没有找到该联系人" << endl;
+        }}
+        return  ;
 }
 void save()
 {
-
+     
 }
 void load()
 {
